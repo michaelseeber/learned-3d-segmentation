296a1da47332db4136271697285e3dd8714141b3
Michael <me@michaelseeber.com>
2018-06-19 00:56:42 +0200

    modified segment.py

--- segmentation/segment.py
+++ segmentation/segment.py
@@ -8,97 +8,43 @@ from model import *
 from pyntcloud import PyntCloud
 
 
-def segment(model_path, data_path):
-    is_training = False
 
-    BATCH_SIZE = 1
-    NUM_POINT = 4096
+def segment(model_path, data_path):
+
+    #TODO INPUT preprocess i.e. center
+    input_data = PyntCloud.from_file(os.path.join(data_path, "scene0000_00_vh_clean_2.ply"))
+    train_data = input_data.points.drop('alpha', axis=1).values
+    input_data.points['x'] = (input_data.points['x'] - input_data.points['x'].mean())
+    input_data.points['y'] = (input_data.points['y'] - input_data.points['y'].mean())
+    input_data.points['z'] = (input_data.points['z'] - input_data.points['z'].mean())
      
     with tf.device('/gpu:0'):
-        pointclouds_pl, labels_pl = placeholder_inputs(BATCH_SIZE, NUM_POINT)
+        pointclouds_pl, _ = placeholder_inputs(BATCH_SIZE)
         is_training_pl = tf.placeholder(tf.bool, shape=())
 
-        # simple model
-        pred = get_model(pointclouds_pl, is_training_pl)
-        loss = get_loss(pred, labels_pl)
-        pred_softmax = tf.nn.softmax(pred)
- 
-        # Add ops to save and restore all the variables.
-        saver = tf.train.Saver()
-        
+        #load model
+        pred = get_model(pointclouds_pl, tc.constant(False))
+
+    saver = tf.train.Saver() 
+
     # Create a session
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     config.allow_soft_placement = True
     config.log_device_placement = True
-    sess = tf.Session(config=config)
-
-    # Restore variables from model_path
-    saver.restore(sess, model_path)
-    log_string("Model restored.")
-
-    ops = {'pointclouds_pl': pointclouds_pl,
-           'labels_pl': labels_pl,
-           'is_training_pl': is_training_pl,
-           'pred': pred,
-           'pred_softmax': pred_softmax,
-           'loss': loss}
     
+    with tf.Session(config=config) as sess:
+        # Restore variables from model_path
+        saver.restore(sess, model_path)
+        log_string("Model restored")
 
-    return eval_one_epoch(sess, ops, data_path)
+        prediction = sess.run(pred, feed_dict={pointclouds_pl:input_data})
 
+    # ops = {'pointclouds_pl': pointclouds_pl,
+    #        'labels_pl': labels_pl,
+    #        'is_training_pl': is_training_pl}
 
-def eval_one_epoch(sess, ops, data_path):
-    is_training = False
+    # labeled_cloud = PytnCloud(pd.DataFrame({'x': pts[:, 6], 'y': pts[:, 7], 'z': pts[:, 8], 'label': pred_label[b, :]}))
 
-    current_data, current_label = indoor3d_util.room2blocks_wrapper_normalized(room_path, NUM_POINT)
-    current_data = current_data[:,0:NUM_POINT,:]
-    current_label = np.squeeze(current_label)
-    # Get room dimension..
-    data_label = np.load(room_path)
-    data = data_label[:,0:6]
-    max_room_x = max(data[:,0])
-    max_room_y = max(data[:,1])
-    max_room_z = max(data[:,2])
-
-    file_size = current_data.shape[0]
-    num_batches = file_size // BATCH_SIZE
-    print(file_size)
-
-    for batch_idx in range(num_batches):
-        start_idx = batch_idx * BATCH_SIZE
-        end_idx = (batch_idx+1) * BATCH_SIZE
-        cur_batch_size = end_idx - start_idx
-        
-        feed_dict = {ops['pointclouds_pl']: current_data[start_idx:end_idx, :, :],
-                        ops['labels_pl']: current_label[start_idx:end_idx],
-                        ops['is_training_pl']: is_training}
-        loss_val, pred_val = sess.run([ops['loss'], ops['pred_softmax']],
-                                        feed_dict=feed_dict)
-
-        # if FLAGS.no_clutter:
-        #     pred_label = np.argmax(pred_val[:,:,0:12], 2) # BxN
-        # else:
-            pred_label = np.argmax(pred_val, 2) # BxN
-        # Save prediction labels to OBJ file
-        for b in range(BATCH_SIZE):
-            pts = current_data[start_idx+b, :, :]
-            l = current_label[start_idx+b,:]
-            pts[:,6] *= max_room_x
-            pts[:,7] *= max_room_y
-            pts[:,8] *= max_room_z
-            pts[:,3:6] *= 255.0
-            pred = pred_label[b, :]
-            # for i in range(NUM_POINT):
-            #     color = indoor3d_util.g_label2color[pred[i]]
-            #     fout.write('v %f %f %f %d %d %d\n' % (pts[i,6], pts[i,7], pts[i,8], color[0], color[1], color[2]))
-            #     # TODO create pointcloud and then voxalize
-
-        #add points to dataframe and create cloud
-        cloud = PytnCloud(pd.DataFrame({'x': pts[:, 6], 'y': pts[:, 7], 'z': pts[:, 8], 'label': pred_label[b, :]}))
-
-        
-   
-
-
-    
+if __name__ == "__main__":
+    segment("", "")
