21a0d16b5e2e8179d0c57c99473dd52b28f84106
Michael Seeber <grosmich@phys.ethz.ch>
2018-07-05 16:04:23 +0200

    implemented blocks

--- segmentation/model.py
+++ segmentation/model.py
@@ -11,18 +11,19 @@ import tf_util
 
 NUM_CLASSES = 41
 
-def placeholder_inputs(batch_size):
-    pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, 6))
-    labels_pl = tf.placeholder(tf.int32, shape=(batch_size))
-    return pointclouds_pl, labels_pl
+def placeholder_inputs(batch_size, num_point):
+    pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, 6))
+    labels_pl = tf.placeholder(tf.int32, shape=(batch_size, num_point))
+    sampleweights_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point))
+    return pointclouds_pl, labels_pl, sampleweights_pl
 
 def get_model(point_cloud, is_training, bn_decay=None):
-    """ ConvNet baseline, input is BxNx3 gray image """
     batch_size = point_cloud.get_shape()[0].value
-
+    num_point = point_cloud.get_shape()[1].value
+    
     input_image = tf.expand_dims(point_cloud, -1)
     # only one batch
-    input_image = tf.expand_dims(input_image, 0)
+    # input_image = tf.expand_dims(input_image, 0)
     # CONV
     net = tf_util.conv2d(input_image, 64, [1,6], padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training, scope='conv1', bn_decay=bn_decay)
@@ -35,15 +36,15 @@ def get_model(point_cloud, is_training, bn_decay=None):
     points_feat1 = tf_util.conv2d(net, 1024, [1,1], padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training, scope='conv5', bn_decay=bn_decay)
     # MAX
-    pc_feat1 = tf_util.max_pool2d(points_feat1, [batch_size,1], padding='VALID', scope='maxpool1')
+    pc_feat1 = tf_util.max_pool2d(points_feat1, [num_point,1], padding='VALID', scope='maxpool1')
     # FC
-    pc_feat1 = tf.reshape(pc_feat1, [1, -1])
+    pc_feat1 = tf.reshape(pc_feat1, [batch_size, -1])
     pc_feat1 = tf_util.fully_connected(pc_feat1, 256, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay)
     pc_feat1 = tf_util.fully_connected(pc_feat1, 128, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay)
     print(pc_feat1)
    
     # CONCAT 
-    pc_feat1_expand = tf.tile(tf.reshape(pc_feat1, [1, 1, 1, -1]), [1, batch_size, 1, 1])
+    pc_feat1_expand = tf.tile(tf.reshape(pc_feat1, [batch_size, 1, 1, -1]), [1, num_point, 1, 1])
     points_feat1_concat = tf.concat(axis=3, values=[points_feat1, pc_feat1_expand])
     
     # CONV 
@@ -52,17 +53,16 @@ def get_model(point_cloud, is_training, bn_decay=None):
     net = tf_util.conv2d(net, 256, [1,1], padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training, scope='conv7')
     net = tf_util.dropout(net, keep_prob=0.7, is_training=is_training, scope='dp1')
-
-    net = tf_util.conv2d(net, NUM_CLASSES, [1,1], padding='VALID', stride=[1,1],
+    net = tf_util.conv2d(net, 13, [1,1], padding='VALID', stride=[1,1],
                          activation_fn=None, scope='conv8')
-    net = tf.squeeze(net)
+    net = tf.squeeze(net, [2])
 
     return net
 
-def get_loss(pred, label):
+def get_loss(pred, label, sampleweight):
     """ pred: B,32
         label: B """
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pred, labels=label)
+    loss = tf.losses.sparse_softmax_cross_entropy(logits=pred, labels=label, weights= sampleweight)
     return tf.reduce_mean(loss)
 
 if __name__ == "__main__":
--- segmentation/train_segmentation.py
+++ segmentation/train_segmentation.py
@@ -24,14 +24,13 @@ if os.path.exists("/scratch/thesis/HIL"):
     ptvsd.enable_attach("thesis", address = ('192.33.89.41', 3000))
     ptvsd.wait_for_attach()
 
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_path', required = True)
 parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
 parser.add_argument('--log_dir', default='model', help='Log dir [default: log]')
+parser.add_argument('--num_point', type=int, default=8192, help='Point number [default: 4096]')
 parser.add_argument('--max_epoch', type=int, default=50, help='Epoch to run [default: 50]')
-parser.add_argument('--batch_size', type=int, default=10000, help='Batch Size during training [default: 3000]')
+parser.add_argument('--batch_size', type=int, default=1, help='Batch Size during training [default: 1]')
 parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
 parser.add_argument('--momentum', type=float, default=0.9, help='Initial learning rate [default: 0.9]')
 parser.add_argument('--optimizer', default='adam', help='adam or momentum [default: adam]')
@@ -41,8 +40,8 @@ parser.add_argument('--decay_rate', type=float, default=0.5, help='Decay rate fo
 FLAGS = parser.parse_args()
 
 
-
 DATA_PATH = FLAGS.data_path
+NUM_POINT = FLAGS.num_point
 BATCH_SIZE = FLAGS.batch_size
 MAX_EPOCH = FLAGS.max_epoch
 BASE_LEARNING_RATE = FLAGS.learning_rate
@@ -52,21 +51,19 @@ OPTIMIZER = FLAGS.optimizer
 DECAY_STEP = FLAGS.decay_step
 DECAY_RATE = FLAGS.decay_rate
 
+BN_INIT_DECAY = 0.5
+BN_DECAY_DECAY_RATE = 0.5
+BN_DECAY_DECAY_STEP = float(DECAY_STEP)
+BN_DECAY_CLIP = 0.99
+
+NUM_CLASSES = 41
+
 LOG_DIR = os.path.join(BASE_DIR, FLAGS.log_dir)
 if not os.path.exists(LOG_DIR): os.mkdir(LOG_DIR)
 # os.system('cp model.py %s' % (LOG_DIR)) # bkp of model def
 # os.system('cp train_segmentation.py %s' % (LOG_DIR)) # bkp of train procedure
 LOG_FOUT = open(os.path.join(LOG_DIR, 'log_train.txt'), 'w')
 LOG_FOUT.write(str(FLAGS)+'\n')
-
-
-BN_INIT_DECAY = 0.5
-BN_DECAY_DECAY_RATE = 0.5
-#BN_DECAY_DECAY_STEP = float(DECAY_STEP * 2)
-BN_DECAY_DECAY_STEP = float(DECAY_STEP)
-BN_DECAY_CLIP = 0.99
-
-NUM_CLASSES = 41
 
 scene_list = []
 with open(os.path.join(DATA_PATH, "list.txt"), "r") as fid:
@@ -85,7 +82,6 @@ for scene in scene_list:
 pointcloud = pd.concat(pointcloud)
 labels = pd.concat(labels)
 
-
 # Center PointCloud
 pointcloud['x'] = (pointcloud['x'] - pointcloud['x'].mean())
 pointcloud['y'] = (pointcloud['y'] - pointcloud['y'].mean())
@@ -99,11 +95,13 @@ labelcolors = labels.drop(['x','y','z','alpha','label'], axis=1).values
 train_label = np.zeros(labelcolors.shape[0], dtype=np.int8)
 for i in range(labelcolors.shape[0]):
     color = (labelcolors[i, 0], labelcolors[i, 1], labelcolors[i, 2])
-    if(label_util.color2label(color) == 40):
-        todelete.append(i)
+    # Delte specific labels
+    # if(label_util.color2label(color) == 40):
+        #  todelete.append(i)
     train_label[i] = label_util.color2label(color)
 train_data = np.delete(train_data, todelete, axis=0)
 train_label = np.delete(train_label, todelete, axis=0)
+
 
 
 #Print statistic about loaded data
@@ -122,10 +120,68 @@ for i in range(unique_items.size):
 # covnert back to scannet id's through SCANNET_MAPPING[train_label]
 # SCANNET_MAPPING, train_label = np.unique(train_label, return_inverse=True)
 
-print(train_label[0:100])
 
-test_data = train_data
-test_label = train_label
+# Preprocessing
+labelweights = np.zeros(NUM_CLASSES)
+tmp, _ = np.histogram(train_label, range(NUM_CLASSES+1))
+labelweights += tmp
+labelweights = labelweights/np.sum(labelweights)
+labelweights = 1/np.log(1.2+labelweights)
+
+def getBlock(index):
+    point_set = train_data[index, 0:3]
+    semantic_seg = train_label[index]
+    coordmax = np.max(point_set, axis=0)
+    coordmin = np.min(point_set, axis=0)
+    samplemin = np.maximum(coordmax-[1.5,1.5,3.0], coordmin)
+    samplemin[2] = coordmin[2]
+    samplesize = np.minimum(coordmax-samplemin,[1.5,1.5,3.0])
+    samplesize = coordmax[2]-coordmin[2]
+    isvalid = False
+    for i in range(10):
+        currcenter = point_set[np.random.choice(len(semantic_seg),1)[0],0:3]
+        currmin = currcenter-[0.75,0.75,1.5]
+        currmax = currcenter+[0.75,0.75,1.5]
+        currmin[2] = coordmin[2]
+        currmax[2] = coordmax[2]
+        currchoice = np.sum((point_set >=(currmin-0.2))*(point_set <=(currmax+0.2)),axis=1)==3
+        curr_point_set = point_set[currchoice,:]
+        curr_semantic_seg = semantic_seg[currchoice]
+        if len(curr_semantic_seg)==0:
+            continue
+        mask = np.sum((curr_point_set >= (currmin-0.01))*(curr_point_set <= (currmax+0.01)),axis=1)==3
+        vidx = np.ceil((curr_point_set[mask,0:3]-currmin)/(currmax-currmin)*[31.0,31.0,62.0])
+        vidx = np.unique(vidx[:,0]*31.0*62.0+vidx[:,1]*62.0+vidx[:,2])
+        isvalid = np.sum(curr_semantic_seg>0)/len(curr_semantic_seg)>=0.7 and len(vidx)/31.0/31.0/62.0>=0.02
+        if isvalid:
+            break
+    choice = np.random.choice(len(curr_semantic_seg), NUM_POINT, replace=True)
+    point_set = curr_point_set[choice,:]
+    semantic_seg = curr_semantic_seg[choice]
+    mask = mask[choice]
+    sample_weight = labelweights[semantic_seg]
+    sample_weight *= mask
+    return point_set, semantic_seg, sample_weight
+
+def get_batch_wdp(idxs, start_idx, end_idx):
+    bsize = end_idx-start_idx
+    batch_data = np.zeros((bsize, NUM_POINT, 6))
+    batch_label = np.zeros((bsize, NUM_POINT), dtype=np.int32)
+    batch_sampleweight = np.zeros((bsize, NUM_POINT), dtype=np.float32)
+    for i in range(bsize):
+        ps,seg,smpw = getBlock(idxs[i+start_idx])
+        batch_data[i,...] = ps
+        batch_label[i,:] = seg
+        batch_sampleweight[i,:] = smpw
+
+    dropout_ratio = np.random.random()*0.875 # 0-0.875
+    drop_idx = np.where(np.random.random((ps.shape[0]))<=dropout_ratio)[0]
+    batch_data[i,drop_idx,:] = batch_data[i,0,:]
+    batch_label[i,drop_idx] = batch_label[i,0]
+    batch_sampleweight[i,drop_idx] *= 0
+    return batch_data, batch_label, batch_sampleweight
+# test_data = train_data
+# test_label = train_label
 
 
 
@@ -158,7 +214,7 @@ def get_bn_decay(batch):
 def train():
     with tf.Graph().as_default():
         with tf.device('/gpu:'+str(GPU_INDEX)):
-            pointclouds_pl, labels_pl = placeholder_inputs(BATCH_SIZE)
+            pointclouds_pl, labels_pl, sampleweights_pl = placeholder_inputs(BATCH_SIZE, NUM_POINT)
             is_training_pl = tf.placeholder(tf.bool, shape=())
             
             # Note the global_step=batch parameter to minimize. 
@@ -169,11 +225,11 @@ def train():
 
             # Get model and loss 
             pred = get_model(pointclouds_pl, is_training_pl, bn_decay=bn_decay)
-            loss = get_loss(pred, labels_pl)
+            loss = get_loss(pred, labels_pl, sampleweights_pl)
             tf.summary.scalar('loss', loss)
 
-            correct = tf.equal(tf.argmax(pred, 1), tf.to_int64(labels_pl))
-            accuracy = tf.reduce_sum(tf.cast(correct, tf.float32)) / float(BATCH_SIZE)
+            correct = tf.equal(tf.argmax(pred, 2), tf.to_int64(labels_pl))
+            accuracy = tf.reduce_sum(tf.cast(correct, tf.float32)) / float(BATCH_SIZE*NUM_POINT)
             tf.summary.scalar('accuracy', accuracy)
 
             # Get training operator
@@ -197,8 +253,7 @@ def train():
 
         # Add summary writers
         merged = tf.summary.merge_all()
-        train_writer = tf.summary.FileWriter(os.path.join(LOG_DIR, 'train'),
-                                  sess.graph)
+        train_writer = tf.summary.FileWriter(os.path.join(LOG_DIR, 'train'), sess.graph)
         test_writer = tf.summary.FileWriter(os.path.join(LOG_DIR, 'test'))
 
         # Init variables
@@ -234,31 +289,35 @@ def train_one_epoch(sess, ops, train_writer):
     is_training = True
     
     log_string('----')
-    current_data, current_label, _ = provider.shuffle_data(train_data, train_label) 
     
-    file_size = current_data.shape[0]
-    num_batches = file_size // BATCH_SIZE
+    idxs = np.arange(len(train_label))
+    np.random.shuffle(idxs)
+
+    num_batches = len(train_label) // BATCH_SIZE 
     
     total_correct = 0
     total_seen = 0
     loss_sum = 0
     
     for batch_idx in range(num_batches):
-        if batch_idx % 10 == 0:
+        if batch_idx % 100 == 0:
             print('Current batch/total batch num: %d/%d'%(batch_idx,num_batches))
         start_idx = batch_idx * BATCH_SIZE
         end_idx = (batch_idx+1) * BATCH_SIZE
+
+        batch_data, batch_label, batch_smpw = get_batch_wdp(idxs, start_idx, end_idx)
         
-        feed_dict = {ops['pointclouds_pl']: current_data[start_idx:end_idx, :],
-                     ops['labels_pl']: current_label[start_idx:end_idx],
+        feed_dict = {ops['pointclouds_pl']: batch_data[start_idx:end_idx, :, :],
+                     ops['labels_pl']: batch_label[start_idx:end_idx],
+                     ops['sampleweights_pl']: batch_smpw,
                      ops['is_training_pl']: is_training,}
         summary, step, _, loss_val, pred_val = sess.run([ops['merged'], ops['step'], ops['train_op'], ops['loss'], ops['pred']],
                                          feed_dict=feed_dict)
         train_writer.add_summary(summary, step)
-        pred_val = np.argmax(pred_val, 1)
+        pred_val = np.argmax(pred_val, 2)
         correct = np.sum(pred_val == current_label[start_idx:end_idx])
         total_correct += correct
-        total_seen += BATCH_SIZE
+        total_seen += (BATCH_SIZE*NUM_POINT)
         loss_sum += loss_val
     
     log_string('mean loss: %f' % (loss_sum / float(num_batches)))
@@ -285,21 +344,22 @@ def eval_one_epoch(sess, ops, test_writer):
         start_idx = batch_idx * BATCH_SIZE
         end_idx = (batch_idx+1) * BATCH_SIZE
 
-        feed_dict = {ops['pointclouds_pl']: current_data[start_idx:end_idx, :],
+        feed_dict = {ops['pointclouds_pl']: current_data[start_idx:end_idx, :, :],
                      ops['labels_pl']: current_label[start_idx:end_idx],
                      ops['is_training_pl']: is_training}
         summary, step, loss_val, pred_val = sess.run([ops['merged'], ops['step'], ops['loss'], ops['pred']],
                                       feed_dict=feed_dict)
         test_writer.add_summary(summary, step)
-        pred_val = np.argmax(pred_val, 1)
+        pred_val = np.argmax(pred_val, 2)
         correct = np.sum(pred_val == current_label[start_idx:end_idx])
         total_correct += correct
-        total_seen += BATCH_SIZE
-        loss_sum += loss_val
+        total_seen += (BATCH_SIZE*NUM_POINT)
+        loss_sum += (loss_val*BATCH_SIZE)
         for i in range(start_idx, end_idx):
-            l = current_label[i]
-            total_seen_class[l] += 1
-            total_correct_class[l] += (pred_val[i-start_idx] == l)
+            for j in range(NUM_POINT):
+                l = current_label[i, j]
+                total_seen_class[l] += 1
+                total_correct_class[l] += (pred_val[i-start_idx, j] == l)
 
     log_string('eval mean loss: %f' % (loss_sum / float(total_seen)))
     log_string('eval accuracy: %f'% (total_correct / float(total_seen)))
