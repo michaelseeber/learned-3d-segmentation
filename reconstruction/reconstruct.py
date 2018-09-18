c86f9f38731c3bb273a03aae5e4ba104e271471b
Michael Seeber <grosmich@phys.ethz.ch>
2018-09-18 14:28:39 +0200

    nearly final

--- reconstruction/reconstruct.py
+++ reconstruction/reconstruct.py
@@ -3,7 +3,8 @@ import glob
 import argparse
 import numpy as np
 import tensorflow as tf
-from train_scannet import pointnet_data_generator, build_model, categorical_crossentropy, classification_accuracy
+from train_scannet import pointnet_data_generator, build_model
+from skimage.measure import marching_cubes_lewiner
 
 
 SEG_POINTCLOUDS_PATH = '/scratch/thesis/data/segmented'
@@ -88,7 +89,7 @@ def evaluate(scene_train_list_path, scene_val_list_path,
                 [tf.argmax(probs[0], axis=-1)],
                 feed_dict=feed_dict
             )
-        
+
 
             
 
@@ -154,7 +155,38 @@ def parse_args():
 
     return parser.parse_args()
 
+def extract_mesh_marching_cubes(path, volume, color=None, level=0.5,
+                                step_size=1.0, gradient_direction="ascent"):
+    if level > volume.max() or level < volume.min():
+        return
 
+    verts, faces, normals, values = marching_cubes_lewiner(
+        volume, level=level, step_size=step_size,
+        gradient_direction=gradient_direction)
+
+    ply_verts = np.empty(len(verts),
+                         dtype=[("x", "f4"), ("y", "f4"), ("z", "f4")])
+    ply_verts["x"] = verts[:, 0]
+    ply_verts["y"] = verts[:, 1]
+    ply_verts["z"] = verts[:, 2]
+    ply_verts = plyfile.PlyElement.describe(ply_verts, "vertex")
+
+    if color is None:
+        ply_faces = np.empty(
+            len(faces), dtype=[("vertex_indices", "i4", (3,))])
+    else:
+        ply_faces = np.empty(
+            len(faces), dtype=[("vertex_indices", "i4", (3,)),
+                               ("red", "u1"), ("green", "u1"), ("blue", "u1")])
+        ply_faces["red"] = color[0]
+        ply_faces["green"] = color[1]
+        ply_faces["blue"] = color[2]
+    ply_faces["vertex_indices"] = faces
+    ply_faces = plyfile.PlyElement.describe(ply_faces, "face")
+
+    with tempfile.NamedTemporaryFile(dir=".", delete=False) as tmpfile:
+        plyfile.PlyData([ply_verts, ply_faces]).write(tmpfile.name)
+        shutil.move(tmpfile.name, path)
 
 
 if __name__ == "__main__":
